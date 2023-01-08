d421faf88ac5e6df0d9b34ea71a820f4bab2108d
pansuriyakeyur <keyurpansuriya893@gmail.com>
2023-01-08 02:16:05 +0100

    code start first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Hero from './Component/Hero/Hero';
+import Programs from './Component/Programs/Programs';
+import Reasons from './Component/Reasons/Reasons';
+import Plans from './Component/Plans/Plans';
+import Testimonials from './Component/Testimonials/Testimonials';
+import Join from './Component/Join/Join';
+import Footer from './Component/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Hero />
+      <Programs />
+      <Reasons />
+      <Plans />
+      <Testimonials />
+      <Join />
+      <Footer />
     </div>
   );
 }
