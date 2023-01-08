f4a73bff9e3080c6c3603753144e7553e9a7e103
pansuriyakeyur <keyurpansuriya893@gmail.com>
2023-01-08 04:20:09 +0100

    final code submit

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 import './Footer.css'
+
+import GitHub from '../../assets/github.png'
+import Instagram from '../../assets/instagram.png'
+import LinkedIn from '../../assets/linkedin.png'
+import Logo from '../../assets/logo1.png'
+
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="Footer-container">
+        <hr />
+        <div className="footer">
+            <div className='social-links'>
+            <img src={GitHub} alt="" />
+            <img src={Instagram} alt="" />
+            <img src={LinkedIn} alt="" />
+            </div>
+        
+
+        <div className="logo-f">
+            <img src={Logo} alt="" />
+        </div>
+        </div>
+
+        <div className="blur blur-f-1"></div>
+        <div className="blur blur-f-2"></div>
+
+    </div>
   )
 }
 
--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -6,18 +6,29 @@ import hero_image from '../../assets/hero_image.png'
 import hero_image_back from '../../assets/hero_image_back.png'
 import Heart from '../../assets/heart.png'
 import Calories from '../../assets/calories.png'
+import NumberCounter from 'number-counter'
+import {motion} from 'framer-motion';
 
 const Hero = () => {
+
+  const transition = {type: 'spring', duration : 3}
+  const mobile = window.innerWidth<=768 ? true: false;
   return (
 
-   <div className="hero">
+   <div className="hero" id='home'>
+
+    <div className="blur hero-blur"></div>
     <div className="left-h">
         <Header />
 
         {/* the best ad */}
 
         <div className="the-best-ad">
-          <div></div>
+          <motion.div
+                      initial={{left: mobile? "165px": '238px'}}
+                      whileInView={{left: '8px'}}
+                      transition={{...transition, type: 'tween'}}
+          ></motion.div>
           <span>the best fitness club in the town</span>
         </div>
 
@@ -42,15 +53,19 @@ const Hero = () => {
 
         <div className="figures">
           <div>
-            <span>140+</span>
+            <span>
+              <NumberCounter end={140} start={100} delay="4" preFix="+"/>
+            </span>
             <span>expert coachs</span>
           </div>
           <div>
-            <span>978+</span>
+            <span><NumberCounter end={978} start={800} delay="4" preFix="+"/></span>
             <span>members joined</span>
           </div>
           <div>
-            <span>50+</span>
+            <span>
+            <NumberCounter end={50} start={30} delay="4" preFix="+"/>
+            </span>
             <span>fitness programs</span>  
           </div>
         </div>
@@ -67,23 +82,28 @@ const Hero = () => {
     <div className="right-h">
         <button className="btn">Join Now</button>
 
-        <div className="heart-rate">
+        <motion.div transition={transition} initial={{right: "-1rem"}} whileInView={{right: "4rem"}} className="heart-rate">
           <img src={Heart} alt="" />
           <span>Heart Rate</span>
           <span>116 bpm</span>
-        </div>
+        </motion.div>
 
         {/* hero images */}
         <img src={hero_image} alt="" className="hero-image" />
-        <img src={hero_image_back} alt="" className="hero-image-back" />
+        <motion.img 
+            transition={transition} initial={{right: "11rem"}} whileInView={{right: "20rem"}}
+        src={hero_image_back} alt="" className="hero-image-back" />
 
         {/* calories */}
-        <div className="calories">
+        <motion.div
+        transition={transition} initial={{right: "37rem"}} whileInView={{right: "28rem"}}
+        className="calories">
           <img src={Calories} alt="" />
           <div>  <span>Calories Burned</span>
-          <span>220 kcal</span></div>
+          <span>220 kcal</span>
+          </div>
         
-        </div>
+        </motion.div>
     </div>
    </div>
   )
